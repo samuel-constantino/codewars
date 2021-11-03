7d3af1cedf8763ff1ab43cc6b6f8cd41925b5aa9
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-03 11:01:14 -0300

    resolve [5 kyu]-Moving-Zeros-To-The-End

---
+++ challenges/[5 kyu]-Moving-Zeros-To-The-End.js
@@ -0,0 +1,26 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, 
+preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+const moveZeros = (arr) => {
+    let indexs = [];
+    let zeros = [];
+    let auxArray = [];
+    arr.forEach((item) => {
+        if (item === 0) {
+            zeros.push(item);
+        } else {
+            auxArray.push(item);
+        }
+    });
+
+    newArray = [...auxArray, ...zeros];
+
+    return newArray;
+};
+
+// console.log(moveZeros([0,0,1]));
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
