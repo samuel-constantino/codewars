eef397ac4bc64b77229faad9c17a5988ce71b135
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-03 11:05:10 -0300

    resolucao do CodeWars

--- challenges/[5 kyu]-Moving-Zeros-To-The-End.js
+++ challenges/[5 kyu]-Moving-Zeros-To-The-End.js
@@ -6,20 +6,24 @@ moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 
 const moveZeros = (arr) => {
-    let indexs = [];
-    let zeros = [];
-    let auxArray = [];
-    arr.forEach((item) => {
-        if (item === 0) {
-            zeros.push(item);
-        } else {
-            auxArray.push(item);
-        }
-    });
+    // let zeros = [];
+    // let auxArray = [];
+    // arr.forEach((item) => {
+    //     if (item === 0) {
+    //         zeros.push(item);
+    //     } else {
+    //         auxArray.push(item);
+    //     }
+    // });
 
-    newArray = [...auxArray, ...zeros];
+    // newArray = [...auxArray, ...zeros];
 
-    return newArray;
+    // return newArray;
+
+    const result = arr.filter((item) => item !== 0)
+        .concat(arr.filter((item) => item === 0));
+
+    return result;
 };
 
 // console.log(moveZeros([0,0,1]));
